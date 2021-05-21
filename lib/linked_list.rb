3bd0351a4a481fc815bde014447a1bead919118a
Jessica Chan <jwinchan@gmail.com>
2021-05-20 19:41:45 -0700

    complete up til get_at_index

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -18,65 +18,117 @@ class LinkedList
 
     # method to add a new node with the specific data value in the linked list
     # insert the new node at the beginning of the linked list
-    # Time Complexity: ?
-    # Space Complexity: ?
+    # Time Complexity: O(1)
+    # Space Complexity: O(1)
     def add_first(value)
-      raise NotImplementedError
+      new_node = Node.new(value, @head)
+      @head = new_node
     end
 
     # method to find if the linked list contains a node with specified value
     # returns true if found, false otherwise
-    # Time Complexity: ?
-    # Space Complexity: ?
+    # Time Complexity: O(n)
+    # Space Complexity: O(1)
     def search(value)
-      raise NotImplementedError
+      current = @head
+      until current.nil?
+        if current.data == value
+          return true
+        else
+          current = current.next
+        end
+      end
+      return false
     end
 
     # method to return the max value in the linked list
     # returns the data value and not the node
+    # Time Complexity: O(n)
+    # Space Complexity: O(1)
     def find_max
-      raise NotImplementedError
+      return nil if @head.nil?
+      current = @head
+      max_value = current.data
+      until current.nil?
+        if current.data > max_value
+          max_value = current.data
+        end
+          current = current.next
+      end
+      return max_value
     end
 
     # method to return the min value in the linked list
     # returns the data value and not the node
-    # Time Complexity: ?
-    # Space Complexity: ?
+    # Time Complexity: O(n)
+    # Space Complexity: O(1)
     def find_min
-      raise NotImplementedError
+      return nil if @head.nil?
+      current = @head
+      min_value = current.data
+      until current.nil?
+        if current.data < min_value
+          min_value = current.data
+        end
+          current = current.next
+      end
+      return min_value
     end
 
 
     # Additional Exercises 
     # returns the value in the first node
     # returns nil if the list is empty
-    # Time Complexity: ?
-    # Space Complexity: ?
+    # Time Complexity: O(1)
+    # Space Complexity: O(1)
     def get_first
-      raise NotImplementedError
+      return nil if @head.nil?
+      return @head.data
     end
 
     # method that inserts a given value as a new last node in the linked list
-    # Time Complexity: ?
-    # Space Complexity: ?
+    # Time Complexity: O(n)
+    # Space Complexity: O(1)
     def add_last(value)
-      raise NotImplementedError
+      current = @head
+      until current.nil?
+        current = current.next
+      end
+      current = Node.new(value)
     end
 
     # method that returns the length of the singly linked list
-    # Time Complexity: ?
-    # Space Complexity: ?
+    # Time Complexity: O(n)
+    # Space Complexity: O(1)
     def length
-      raise NotImplementedError
+      length = 0
+      current = @head
+      until current.nil?
+        length += 1
+        current = current.next
+      end
+      return length
     end
 
     # method that returns the value at a given index in the linked list
     # index count starts at 0
     # returns nil if there are fewer nodes in the linked list than the index value
-    # Time Complexity: ?
-    # Space Complexity: ?
+    # Time Complexity: O(n) 
+    # Space Complexity: O(1)
+
     def get_at_index(index)
-      raise NotImplementedError
+      return nil if @head.nil?
+      index_counter = 0
+      current = @head
+      until index_counter == index || current.nil?
+        current = current.next
+        index_counter += 1
+      end
+      if index_counter < index
+        return nil
+      else 
+        return current.data
+      end
     end
 
     # method to print all the values in the linked list
